7da7a81ed5c19979b672de84f2bbf2a30e181ff9
adrian gallero <agallero@gmail.com>
2025-08-03 08:23:24 -0300

    Added a testapp to the install-everything test
    Now, not only we install everything, but we also compile a small app that uses flexcel, biz.core, scripter, spring4d and tms crypto. The app is both compiled with smartsetup (to test compiling an exe with deps), and also standalone with msbuild (to test if they were installed right)

--- tmstest/tests/build/install-everything/test.slow.build.install-everything.ps1
+++ tmstest/tests/build/install-everything/test.slow.build.install-everything.ps1
@@ -4,7 +4,32 @@
 
 . test.setup
 
-  tms credentials -code:$env:TMSTEST_CODE -email:$env:TMSTEST_EMAIL
-  tms install *    
-  tms uninstall * #only if no errors. If there are errors, we leave it there for investigation.
+# Duplicate the testapp. We will test it with smartsetup and with msbuild (to see if the paths are ok)
+Copy-Item -Path testapp -Destination testapp-no-tmsbuild -Exclude tmsbuild.yaml
+
+tms credentials -code:$env:TMSTEST_CODE -email:$env:TMSTEST_EMAIL
+
+#tms fetch *   #do not install, so we can build with the testapp at the same time and see deps work fine.
+tms fetch  tms.flexcel.vcl tms.biz.aurelius tms.vcl.crypto sglienke.spring4d tms.biz.scripter tms.fnc.core
+
+tms build #this will also build testapp.
+
+tms update
+
+$expectedResult = "1DC22C912262B3FDA386381714E4674E240860FB66DD5A70307F800CFA4A6B6F"
+$Result = & testapp\Win32\Debug\testapp.exe
+if ($Result -ne $expectedResult) {
+    Write-Error "Install everything failed: Result was '$Result' and should have been '$expectedResult'."
+}
+
+Set-Location testapp-no-tmsbuild
+msbuild
+$Result = & Win32\Debug\testapp.exe
+if ($Result -ne $expectedResult) {
+    Write-Error "Install everything failed at msbuild: Result was '$Result' and should have been '$expectedResult'."
+}
+
+tms uninstall * #only if no errors. If there are errors, we leave it there for investigation.
+
+#check registry is the same as the one we used to install.
 
