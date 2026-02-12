738d870107dfdda8f3fec7286c6b03cacae89255
adrian gallero <agallero@gmail.com>
2026-02-12 09:59:03 -0300

    Added a specific version of tms.biz.vcl to a test
    This test was installing aurelius 5.19, but due to changes in bcl, it won't compile anymore with the latest bcl. So we now tell it explicitly what bcl to fetch.

--- tmstest/tests/versioning/dont-reinstall/test.versioning.dont-reinstall.ps1
+++ tmstest/tests/versioning/dont-reinstall/test.versioning.dont-reinstall.ps1
@@ -5,10 +5,10 @@ tms server-enable community
 tms server-add testserver zipfile "file:///$($tmsTestRootDir.Replace('\', '/'))/tmp-run/test-repos/tmsbuild_test_repos.zip"
 
 
-$BuildResult = tms install tms.biz.aurelius:5.19 tmstest.a:v1.0.0
+$BuildResult = tms install tms.biz.aurelius:5.19 tms.biz.bcl:1.42 tmstest.a:v1.0.0
 Test-BuildResultCounts -BuildResult $BuildResult -expectedNotModifiedCount 0 -expectedIgnoreCount 0 -expectedOkCount 8
 
-$BuildResult = tms install tms.biz.aurelius:5.19 tmstest.a:v1.0.0
+$BuildResult = tms install tms.biz.aurelius:5.19 tms.biz.bcl:1.42 tmstest.a:v1.0.0
 
 Test-BuildResultCounts -BuildResult $BuildResult -expectedNotModifiedCount 8 -expectedIgnoreCount 0 -expectedOkCount 0
 Test-FetchResultCounts -FetchResult $BuildResult -expectedLines @("*tms.biz.aurelius -> SKIPPED (Up to date)*", "**tms.biz.bcl -> SKIPPED (Up to date)*")
